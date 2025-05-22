330fba78717f8daa6f2c92eef926dbaa93f20973
a23diemujper <a23diemujper@inspedralbes.cat>
2025-05-22 11:19:23 +0200

    prueba4

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
+  publicPath: '/gtaction/', // Aplicación en un subdirectorio llamado "vue"
   transpileDependencies: true
 })
